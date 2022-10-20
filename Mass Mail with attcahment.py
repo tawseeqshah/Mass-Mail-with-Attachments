6ad4aa68d58af8b19c08baebbd11d41d43f9a93b
tawseeqshah <116233019+tawseeqshah@users.noreply.github.com>
2022-10-20 10:21:23 +0530

    Update and rename Internshipmail.py to Mass Mail with attcahment.py

--- Mass Mail with attcahment.py
+++ Mass Mail with attcahment.py
@@ -11,7 +11,7 @@ from email.mime.base import MIMEBase
 #Mails list print.......................................................................................
 
 mails = pd.read_excel(
-    "D:\\Atom\\Python\\Projects Python\\Email Sending\\Student List.xlsx")
+    "File_path")
 mail_column = mails.get("Email")
 listmails = list(mail_column)
 newlist = [x for x in listmails if pd.isnull(x) == False]
@@ -19,7 +19,7 @@ print(f"Sending mass mail to {newlist}.\n")
 
 #Topic 1
 mails = pd.read_excel(
-    "D:\\Atom\\Python\\Projects Python\\Email Sending\\Student List.xlsx")
+    "File_path")
 top1_column = mails.get("Topic 1")
 listtop1 = list(top1_column)
 top1list = [x for x in listtop1 if pd.isnull(x) == False]
@@ -27,7 +27,7 @@ print(top1list)
 
 #Topic 2
 mails = pd.read_excel(
-   "D:\\Atom\\Python\\Projects Python\\Email Sending\\Student List.xlsx")
+   "File_path")
 top2_column = mails.get("Topic 2")
 listtop2 = list(top2_column)
 top2list = [x for x in listtop2 if pd.isnull(x) == False]
@@ -35,7 +35,7 @@ print(top2list)
 
 #NAmes
 mails = pd.read_excel(
-   "D:\\Atom\\Python\\Projects Python\\Email Sending\\Student List.xlsx")
+   "File_path")
 name_column= mails.get("Name")
 namelist = list(name_column)
 name1list = [x for x in namelist if pd.isnull(x) == False]
@@ -43,7 +43,7 @@ print(name1list)
 
 # Designation
 mails = pd.read_excel(
-    "D:\\Atom\\Python\\Projects Python\\Email Sending\\Student List.xlsx")
+    "File_path")
 Designation_column= mails.get("Designation")
 Designationlist = list(Designation_column)
 Designation1list = [x for x in Designationlist if pd.isnull(x) == False]
@@ -53,14 +53,14 @@ for i in range (len(top1list)):
     server = sm.SMTP("smtp.gmail.com", port=587)
 
     server.starttls()
-    server.login("tawseeq_33btech20@nitsri.net", "Ta@taw32seeq01")
+    server.login("from_mail_id", "password")
 
-    from_ = "tawseeq_33btech20@nitsri.net"
+    from_ = "from_mail_id"
     to_ = newlist[i]
     message = MIMEMultipart("alternative")
     message['X-Priority'] = '1'
-    message["Subject"] = "Exploring research internship opportunity for Winter 2022-2023"
-    message["From"] = "tawseeq_33btech20@nitsri.net"
+    message["Subject"] = "My subject"
+    message["From"] = "from_mail_id"
     print(f"Sending {i+1}st/th mail to {newlist[i]}.\n")
 
     html = f'''
@@ -75,24 +75,12 @@ for i in range (len(top1list)):
     <p>Hope this email finds you well. I am <b>Tawseeq Mushtaq Shah</b>, a 3rd-year
     (5th semester) Mechanical Engineering undergraduate at the <b>National
     Institute of Technology Srinagar</b>.<p/>
-
-    <p> I am writing this email in pursuit of an opportunity to work as a
-    <b>research intern</b> under your eminent guidance for the upcoming <b>winter of
-    2023</b>.<p/>
-
+    
     <p/>I am veritably interested in mechanical engineering, especially
-    pertaining to <b>{top1list[i]}</b>. I've been scrutinizing <b>{top2list[i]}</b> thoroughly. I have also read several
-    publications of yours.<p/>
+    interested in <b>{top1list[i]}</b> and <b>{top2list[i]}</b> thoroughly.<p/>
 
-    <p>Recently, I went through your conspicuous work and  I was really
-    intrigued by your participation and benefaction in the field. If
-    possible, I would love to work under your esteemed supervision and am
-    willing to work on any project that will help me fathom the dynamics
-    of materials.<p/>
-
-    <p>Please find my <b>CV attached</b> with this mail for your perusal.
-    I look forward to hearing from you about my candidacy.<p/>
-
+    <p>Please find my <b>CV attached</b> with this mail for your perusal.<p/>
+    
     <p>Thank you for your time and consideration.<p/>
     <p>Sincerely,<p/>
     <p>Tawseeq Mushtaq Shah<p/>
@@ -103,7 +91,7 @@ for i in range (len(top1list)):
 
     text = MIMEText(html, "html")
     message.attach(text)
-    attach_file_name = "D:\Atom\Python\Projects Python\Email Sending\Tawseeq's Resume (4).pdf"
+    attach_file_name = "attachment.pdf"
     attach_file = open(attach_file_name, 'rb') # Open the file as binary mode
 
     payload = MIMEBase('application', 'octet-stream')
